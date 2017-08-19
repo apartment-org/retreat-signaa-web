f8132e415a351134f6fe0459496a5d9910c0f0c3
sidhavratha <sidhavratha@gmail.com>
2017-08-19 22:50:40 +0530

    ui icon dynamic rows and spacing

--- src/app/App.js
+++ src/app/App.js
@@ -2,21 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Activity from './component/activity';
+import ActivityPanel from './component/activityPanel';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {GridList, GridTile} from 'material-ui/GridList';
-
-const muiTheme = getMuiTheme({});
 
 const styles = {
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-  },
-  gridList: {
-    width: 500,
-    height: 450,
     overflowY: 'auto',
   },
 };
@@ -25,25 +16,13 @@ export default class App extends Component {
   render() {
     return (
       <main style={styles.root}>
-        <MuiThemeProvider muiTheme={muiTheme}>
-          <GridList cellHeight={180} style={styles.gridList}>
-            <GridTile>
-              <Activity classes="fa fa-gamepad"/>
-            </GridTile>
-            <GridTile>
-              <Activity classes="fa fa-address-book"/>
-            </GridTile>
-            <GridTile>
-              <Activity classes="fa fa-meetup"/>
-            </GridTile>
-            <GridTile>
-              <Activity classes="fa fa-bicycle"/>
-            </GridTile>
-            <GridTile>
-              <Activity classes="fa fa-windows"/>
-            </GridTile>
-          </GridList>
-        </MuiThemeProvider>
+          <ActivityPanel>
+            <Activity classes="fa fa-gamepad"/>
+            <Activity classes="fa fa-address-book"/>
+            <Activity classes="fa fa-meetup"/>
+            <Activity classes="fa fa-bicycle"/>
+            <Activity classes="fa fa-windows"/>
+          </ActivityPanel>
       </main>
     );
   }
---
+++ src/app/component/activityPanel.js
@@ -0,0 +1,73 @@
+import React, {PureComponent, PropTypes} from 'react';
+
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {GridList, GridTile} from 'material-ui/GridList';
+import {green100, green500, green700} from 'material-ui/styles/colors';
+import 'font-awesome/css/font-awesome.css';
+import $ from 'jquery';
+
+const styles = {
+  root: {
+    margin: '2px',
+    height: '100%',
+    overflowY: 'hidden',
+    overflowX: 'hidden',
+  },
+  gridList: {
+  	display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  	overflowY: 'hidden',
+    overflowX: 'hidden',
+  },
+  gridTile: {
+  	height: '120px',
+    width: '100%',
+    marginTop: '2px',
+    paddingTop: '20px',
+  },
+};
+
+const muiTheme = getMuiTheme({});
+
+export default class activityPanel extends PureComponent {
+	render(){
+		const activityChild = React.Children.map(this.props.children, child => {
+			      return (
+			      		<GridTile style={styles.gridTile} className='tile'>
+			      			{child}
+			      		</GridTile>
+		      		);
+			    });
+		return(
+			<MuiThemeProvider muiTheme={muiTheme}>
+				<div style={styles.root}>
+					<GridList 
+						cellHeight='auto' 
+						style={styles.gridList} 
+						padding={4}
+						cols={(this.state.width*0.8)/120}>
+						{activityChild}
+					</GridList>
+				</div>
+        	</MuiThemeProvider>
+		)
+	}
+
+	updateDimensions() {
+        this.setState({width: $(window).width(), height: $(window).height()});
+    }
+
+    componentWillMount() {
+        this.updateDimensions();
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", this.updateDimensions.bind(this));
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateDimensions.bind(this));
+    }
+}
